8def417c62f156b6f659665c1db7f9a234e562e8
selvam palanimalai <selvam.palanimalai@gmail.com>
2024-10-05 10:54:30 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Installation
+
+
+```bash
+cp env.example .env
+poetry config virtualenvs.in-project true
+poetry run e2b_test.py
+```
+
---
+++ e2b_test.py
@@ -0,0 +1,14 @@
+from e2b_code_interpreter import CodeInterpreter
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+with CodeInterpreter(
+    api_key=os.getenv("E2B_API_KEY")
+) as code_interpreter:
+    code_interpreter.notebook.exec_cell("x = 1")
+    execution = code_interpreter.notebook.exec_cell("x+=1; x")
+    print(execution.text)  # outputs 2
